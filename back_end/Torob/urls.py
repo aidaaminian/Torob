46fd70e9e8005c654e01c52b36600093a8a13749
Aida Aminian <71328742+aidaaminian@users.noreply.github.com>
2022-06-30 21:14:46 +0430

    Implement shop reports

--- back_end/Torob/urls.py
+++ back_end/Torob/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import CreateComplaint
+from .views import CreateComplaint, get_shop_report
 
 urlpatterns = [
-    path('feedback/complaints/<str:complaint_type>', csrf_exempt(CreateComplaint.as_view())),
+    path('feedback/complaints', csrf_exempt(CreateComplaint.as_view())),
+    path('reports/<int:shop_id>', get_shop_report),
 ]
--- back_end/Torob/views.py
+++ back_end/Torob/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from Torob.models import Complaint
 from Torob.serializers import ComplaintSerializer
 
 
 class CreateComplaint(CreateAPIView):
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
         serializer = ComplaintSerializer(data=request.data)
@@ -15,3 +19,11 @@ class CreateComplaint(CreateAPIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response({'message': serializer.errors}, status=400)
+
+
+@api_view(['GET'])
+@login_required
+def get_shop_report(request, shop_id):
+    comps = Complaint.objects.filter(shop_id=shop_id)
+    serializer = ComplaintSerializer(comps, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
