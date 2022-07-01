0b42e977f11443d28e0751ce689c07a53cae9695
Aida Aminian <71328742+aidaaminian@users.noreply.github.com>
2022-07-01 23:55:55 +0430

    Update browse views

--- back_end/browse/urls.py
+++ back_end/browse/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import get_products, get_single_product, get_single_shop, search_product_by_name
+from .views import get_products, get_single_product, get_single_shop, search_product_by_name, get_category_products, \
+    get_head_products
 
 urlpatterns = [
     path('browse/<str:head>/<str:category>/<str:sub_category>/', get_products),
+    path('browse/<str:head>/<str:category>/', get_category_products),
+    path('browse/<str:head>/', get_head_products),
     path('products/<int:product_id>/', get_single_product),
     path('shops/<int:shop_id>/', get_single_shop),
     path('search/', search_product_by_name),
